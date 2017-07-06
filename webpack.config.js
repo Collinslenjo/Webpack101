eb9d199cb39f98ce4e66ea6ebc0b6605a61aec78
Collins_lenjo <collinslenjo.cl@gmail.com>
2017-07-06 11:47:56 +0300

    rewritten webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,22 @@
-const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './views/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
 module.exports = {
-  entry: path.join(__dirname, 'src/index.js'),
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.resolve('dist'),
     filename: 'index.bundle.js'
   },
   module: {
-  	loaders: [
-  	{
-	  test: /\.html$/,
-	  loader: 'html-loader'
-	},
-	{
-	  test: /\.css$/,
-	  loaders: ['style-loader', 'css-loader']
-	},
-	{
-	  test: /\.js$/,
-	  exclude: /node_modules/,
-	  loader: 'babel-loader',
-	  query: {
-	    presets: ['es2015']
-	  }
-	}
-  ]}
-};
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ['style-loader','css-loader'], exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
